4ea24a313106922c994e27bc01d2e0e4b945f5d7
RazaKhan <raza.khan4336@gmail.com>
2022-10-13 00:27:57 +0100

    Install FontAwesome For Icons

--- src/component/todo.js
+++ src/component/todo.js
@@ -1,6 +1,8 @@
 import React from "react";
 import todo from "../Images/todo.jpg";
 import "../App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 const Todo = () => {
   return (
     <>
@@ -11,6 +13,15 @@ const Todo = () => {
 
             <figcaption>Add Your List Here ✌✌</figcaption>
           </figure>
+          <div className="addItems">
+            <input type="text" placeholder="📝 Add Items..." />
+            {/* <i className="fa-solid fa-plus add-btn" title="Add Item"></i> */}
+            <FontAwesomeIcon
+              icon={faPlus}
+              className="fa fa-plus add-btn"
+              title="Add Item"
+            ></FontAwesomeIcon>
+          </div>
         </div>
       </div>
     </>
